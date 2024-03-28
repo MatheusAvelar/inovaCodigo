b1461a245357cb745c91d55d01f75180b7cc4a7b
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-03-28 09:10:44 -0300

    Apropriação de Horas

--- criar_acesso.php
+++ criar_acesso.php
@@ -4,54 +4,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Acesso</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="img/ico.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+    <title>Inova Código - Apropriação de Horas - Controle de Demandas</title>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1516963750502427" crossorigin="anonymous"></script>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        
-        form {
-            max-width: 400px;
-            margin: 0 auto;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-        
-        form label {
-            display: block;
-            margin-bottom: 5px;
-            font-weight: bold;
-        }
-        
-        form input[type="text"],
-        form input[type="email"],
-        form input[type="password"] {
+        table {
+            border-collapse: collapse;
             width: 100%;
+        }
+
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
             padding: 8px;
-            margin-bottom: 10px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        input[type="time"] {
+            width: 100px;
+        }
+        input[type="date"] {
+            width: 120px;
+        }
+
+        form input,
+        form textarea {
+            width: 90%; 
+            padding: 10px;
+            margin-bottom: 15px;
             border: 1px solid #ccc;
             border-radius: 4px;
-            box-sizing: border-box;
+            font-size: 16px;
         }
-        
-        form button {
-            width: 100%;
-            padding: 10px;
-            background-color: #007bff;
-            color: #fff;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-        
-        form button:hover {
-            background-color: #0056b3;
+
+        .delete-cell {
+            text-align: center;
         }
     </style>
 </head>
-
 <body>
+    <header class="navbar">
+        <nav class="nav-links">
+            <a href="index.html">Home</a>
+            <a href="projetos.html">Projetos</a>
+            <a href="publicacoes.html">Publicações</a>
+            <a href="gerador.html">Geradores</a>
+            <a href="projetos/controleFinanceiro/login.php">Controle Financeiro</a>
+            <a href="kanban.html">Kanban</a>
+            <a href="jogos.html">Jogos</a>
+            <a href="https://matheusavelar.github.io/">Currículo</a>
+            <a href="apropriacao.php">Apropriação de Horas</a>
+        </nav>
+    </header>
+    
     <center>
         <h1>Cadastro do Acesso</h1>
     </center>
