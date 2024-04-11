67420319ddcbcf42a732174d6d7fb75982db61fc
Matheus <matheus_valladao@hotmail.com>
2024-04-11 14:37:58 -0300

    Web

--- webscraping.php
+++ webscraping.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Executar script Python com AJAX</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
-<body>
-    <div id="output"></div>
-    <script>
-        $(document).ready(function(){
-            $.ajax({
-                url: 'seu_script.py',
-                success: function(response) {
-                    $('#output').html('<pre>' + response + '</pre>');
-                },
-                error: function(xhr, status, error) {
-                    $('#output').text('Erro ao executar o script Python.');
-                }
-            });
-        });
-    </script>
-</body>
-</html>
+<?php
+// Executa o script Python
+$output = shell_exec('python seu_script.py');
+echo "<pre>$output</pre>";
+?>
