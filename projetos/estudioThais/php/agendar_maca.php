12e408a681a2fad08a6a6e3b854085756cdfac9b
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-06 18:46:36 -0300

    Agendamento de Macas

--- projetos/estudioThais/php/agendar_maca.php
+++ projetos/estudioThais/php/agendar_maca.php
@@ -94,6 +94,6 @@ $conn->close();
 echo "<script>
     sessionStorage.setItem('status', '" . addslashes($status) . "');
     sessionStorage.setItem('message', '" . addslashes($message) . "');
-    window.location.href = 'agendamento.php';
+    window.location.href = 'projetos/estudioThais/agendamento.php';
 </script>";
 ?>
