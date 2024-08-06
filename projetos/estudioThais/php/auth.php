7b624bde56d1103ece038d21ec14a9b7a71349c0
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-06 19:13:40 -0300

    Agendamento de Macas

--- projetos/estudioThais/php/auth.php
+++ projetos/estudioThais/php/auth.php
@@ -5,7 +5,7 @@ $conexao = mysqli_connect("127.0.0.1", "u221588236_root", "Camila@307", "u221588
 
 $email   = $_POST['username'];
 $senha   = md5($_POST['password']);
-$query   = mysqli_query($conexao,"SELECT * FROM usuario WHERE email = '$email' and senha = '$senha'");
+$query   = mysqli_query($conexao,"SELECT * FROM usuarioEstudio WHERE email = '$email' and senha = '$senha'");
 $row     = mysqli_num_rows($query);
 
 if($row>0){ 
