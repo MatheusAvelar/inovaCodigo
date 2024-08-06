cd4bbed8568915a9b902777e9318fc4c62859dd0
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-06 19:18:32 -0300

    Agendamento de Macas

--- projetos/estudioThais/horarios_agendados.php
+++ projetos/estudioThais/horarios_agendados.php
@@ -3,7 +3,7 @@ session_start();
 
 // Verifica se o usuário está logado, se não, redireciona para a página de login
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("Location: ../login.php");
+    header("Location: login.php");
     exit;
 }
 ?>
