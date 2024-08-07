d36404dd3ba09f81ac733e7596617d092bf0ad79
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-07 20:14:27 -0300

    Agendamento de Macas

--- projetos/estudioThais/php/recuperar_senha.php
+++ projetos/estudioThais/php/recuperar_senha.php
@@ -54,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     echo "<script>
-        console.log('" . addslashes($message) . "');
+        console.log('" . addslashes($status) . addslashes($message) . "');
         sessionStorage.setItem('status', '" . addslashes($status) . "');
         sessionStorage.setItem('message', '" . addslashes($message) . "');
         //window.location.href = '../esqueceu_senha.php';
