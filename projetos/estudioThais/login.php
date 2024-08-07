762ab61cfd92e37e11498746ccc23fdae784e818
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-07 15:54:07 -0300

    Agendamento de Macas

--- projetos/estudioThais/login.php
+++ projetos/estudioThais/login.php
@@ -6,13 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendamento de Macas</title>
     <link rel="stylesheet" href="css/style.css">
-    <style>
-        .button-container {
-            display: flex;
-            justify-content: space-between;
-            gap: 10px;
-        }
-    </style>
 </head>
 
 <body>
