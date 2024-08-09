b05fffb97987eb932e31210685ae2fd76a18d8c0
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-09 18:59:14 -0300

    Agendamento de Macas

--- projetos/estudioThais/usuarios_estudio.php
+++ projetos/estudioThais/usuarios_estudio.php
@@ -35,7 +35,6 @@ unset($_SESSION['status'], $_SESSION['message']);
             <ul> 
                 <?php if ($perfil_id == 2) : ?>
                     <li><a href="criar_acesso.php">Criar Acesso</a></li>
-                    <li><a href="usuarios_estudio.php">Usuários</a></li>
                 <?php endif; ?>
                 <li><a href="agendamento.php">Agendamentos</a></li>
                 <li><a href="horarios_agendados.php">Horários Agendados</a></li>
