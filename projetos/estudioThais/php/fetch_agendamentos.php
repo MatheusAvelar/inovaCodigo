23c763024d7e81a9fa883837c65df06959249dd5
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-10 20:53:05 -0300

    Agendamento de Macas

--- projetos/estudioThais/php/fetch_agendamentos.php
+++ projetos/estudioThais/php/fetch_agendamentos.php
@@ -68,9 +68,9 @@ if ($result->num_rows > 0) {
         echo "<td>" . $formattedStartTime . "</td>";
         echo "<td>" . $formattedEndTime . "</td>";
 
-        echo "<script>
-                    alert('Debug Info: ID do usuário logado = " . $_SESSION['id'] . " Usuário SQL: " . $row['usuario_id']." Perfil: " . $perfil_id . "');
-              </script>";
+        // Debug para verificar a consulta SQL
+        debugAlert($perfil_id, 'Perfil: ');
+
         // Verificação para mostrar o botão de excluir apenas se o usuário logado é o dono do agendamento
         if ($row['usuario_id'] == $_SESSION['id'] || $perfil_id == 2) {
             // Verifica se a data do agendamento está a pelo menos 2 dias no futuro
---
+++ projetos/estudioThais/php/utils.php
@@ -0,0 +1,19 @@
+<?php
+// utils.php
+
+/**
+ * Função para exibir uma mensagem de depuração usando alert em JavaScript
+ *
+ * @param mixed $data Dados para depurar. Pode ser uma string, array, ou objeto.
+ * @param string $message Mensagem opcional para exibir antes dos dados.
+ */
+function debugAlert($data, $message = '') {
+    $debugJson = json_encode($data);
+    $message = addslashes($message);
+    $debugJson = addslashes($debugJson);
+    
+    echo "<script>
+            alert('$message: $debugJson');
+          </script>";
+}
+?>
