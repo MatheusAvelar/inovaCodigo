37ca6bc01938c73fc1b123e2d15ec14ddfc85038
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-11 15:48:47 -0300

    Agendamento de Macas

--- projetos/estudioThais/php/agendar_maca.php
+++ projetos/estudioThais/php/agendar_maca.php
@@ -86,8 +86,8 @@ if ($errors) {
         } else {
             // Inserir no banco de dados se não houver conflitos
             $usuarioId = $_SESSION['id']; // ID do usuário logado
-            $stmt = $conn->prepare("INSERT INTO agendamentos (nome_cliente, estilo, tamanho, valor, forma_pagamento, sinal_pago, descricao, maca_id, data, start_time, end_time, usuario_id, email_cliente, status, telefone_cliente) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $stmt->bind_param("ssssssssssssss", $nomeCliente, $estilo, $tamanho, $valor, $formaPagamento, $sinalPago, $descricao, $maca, $date, $startTime, $endTime, $usuarioId, $emailCliente, $status, $telefoneCliente);
+            $stmt = $conn->prepare("INSERT INTO agendamentos (nome_cliente, estilo, tamanho, valor, forma_pagamento, sinal_pago, descricao, maca_id, data, start_time, end_time, usuario_id, email_cliente, status, telefone_cliente) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("sssssssssssssss", $nomeCliente, $estilo, $tamanho, $valor, $formaPagamento, $sinalPago, $descricao, $maca, $date, $startTime, $endTime, $usuarioId, $emailCliente, $status, $telefoneCliente);
 
             if (!$stmt->execute()) {
                 $_SESSION['status'] = "error";
