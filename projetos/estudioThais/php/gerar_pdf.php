e7a09a8aaaaf239d8f7e849f83c3c88842d3903e
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-08-17 15:09:08 -0300

    Agendamento de Macas

--- projetos/estudioThais/php/gerar_pdf.php
+++ projetos/estudioThais/php/gerar_pdf.php
@@ -5,7 +5,6 @@ class PDF extends FPDF
 {
     function Header()
     {
-        // Alinhar a imagem no centro
         $this->Image('../img/tatto.jpeg', 10, 6, 30);  // Caminho relativo para a imagem
         $this->SetFont('Arial', 'B', 16);
         $this->Cell(0, 10, 'Termo de Autorizacao - Estudio de Tatuagem', 0, 1, 'C');
@@ -52,33 +51,36 @@ $assinatura_responsavel = $_POST['assinatura_responsavel'];
 
 // Adicionar conteúdo ao PDF
 $pdf->ChapterTitle('Declaração de Autorização');
-$pdf->ChapterBody("Eu, abaixo identificado, declaro que no gozo pleno de minhas faculdades mentais e psíquicas pelo presente e na melhor forma de direito, autorizo o(a) artista a executar sobre meu corpo ou de meu/minha filho(a) menor nascido, abaixo identificado, que em minha companhia reside e pelo qual sou inteiramente responsável a prática da tatuagem...");
-$pdf->Ln(10);
+$pdf->ChapterBody(
+    "Eu, abaixo identificado, declaro que no gozo pleno de minhas faculdades mentais e psíquicas pelo presente e na melhor forma de direito, autorizo o(a) artista a executar sobre meu corpo ou de meu/minha filho(a) menor nascido, abaixo identificado, que em minha companhia reside e pelo qual sou inteiramente responsável a prática da tatuagem.\n\n" .
+    "Assumo na qualidade de genitor(a) do(a) menor, plena responsabilidade pelo trabalho ora autorizado. É de minha livre vontade declarar que isento de responsabilidade civil ou criminal ao tatuador(a), seja de ordem médica, estética ou ainda defeitos da própria inscrição, salvo aquelas decorrentes de imperícia técnica. Ficando ainda plenamente ciente de que o procedimento da tatuagem tem caráter permanente, não podendo ser removida.\n\n" .
+    "Declaro ainda, ser do meu conhecimento as técnicas a serem executadas, os materiais a serem utilizados, bem como fui informado e tenho total ciência dos procedimentos e cuidados que devem ser executados por mim ou por meu/minha filho(a) durante o período recomendado pelo tatuador, com a finalidade de evitar qualquer complicação no período de cicatrização do local. Reconheço finalmente que a tatuagem se trata de um processo artesanal e como tal não comporta perfeição."
+);
 
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(0, 10, 'Informações do Responsável', 0, 1, 'L');
+$pdf->Cell(0, 10, utf8_decode('Informações do Responsável'), 0, 1, 'L');
 $pdf->SetFont('Arial', '', 12);
-$pdf->Cell(0, 10, 'Nome: ' . utf8_decode($nome_responsavel), 0, 1);
-$pdf->Cell(0, 10, 'RG: ' . utf8_decode($rg_responsavel), 0, 1);
-$pdf->Cell(0, 10, 'CPF: ' . utf8_decode($cpf_responsavel), 0, 1);
-$pdf->Cell(0, 10, 'Data de Nascimento: ' . date('d/m/Y', strtotime($nascimento_responsavel)), 0, 1);
+$pdf->Cell(0, 10, utf8_decode('Nome: ' . $nome_responsavel), 0, 1);
+$pdf->Cell(0, 10, utf8_decode('RG: ' . $rg_responsavel), 0, 1);
+$pdf->Cell(0, 10, utf8_decode('CPF: ' . $cpf_responsavel), 0, 1);
+$pdf->Cell(0, 10, utf8_decode('Data de Nascimento: ' . date('d/m/Y', strtotime($nascimento_responsavel))), 0, 1);
 
 if ($isMenor === 'sim') {
     $pdf->Ln(10);
     $pdf->SetFont('Arial', 'B', 12);
-    $pdf->Cell(0, 10, 'Informações do Menor', 0, 1, 'L');
+    $pdf->Cell(0, 10, utf8_decode('Informações do Menor'), 0, 1, 'L');
     $pdf->SetFont('Arial', '', 12);
-    $pdf->Cell(0, 10, 'Nome: ' . utf8_decode($nome_menor), 0, 1);
-    $pdf->Cell(0, 10, 'RG: ' . utf8_decode($rg_menor), 0, 1);
-    $pdf->Cell(0, 10, 'CPF: ' . utf8_decode($cpf_menor), 0, 1);
-    $pdf->Cell(0, 10, 'Data de Nascimento: ' . date('d/m/Y', strtotime($nascimento_menor)), 0, 1);
+    $pdf->Cell(0, 10, utf8_decode('Nome: ' . $nome_menor), 0, 1);
+    $pdf->Cell(0, 10, utf8_decode('RG: ' . $rg_menor), 0, 1);
+    $pdf->Cell(0, 10, utf8_decode('CPF: ' . $cpf_menor), 0, 1);
+    $pdf->Cell(0, 10, utf8_decode('Data de Nascimento: ' . date('d/m/Y', strtotime($nascimento_menor))), 0, 1);
 }
 
 $pdf->Ln(10);
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(0, 10, 'Assinatura', 0, 1, 'L');
+$pdf->Cell(0, 10, utf8_decode('Assinatura'), 0, 1, 'L');
 $pdf->SetFont('Arial', '', 12);
-$pdf->Cell(0, 10, 'Assinatura do Responsável: ' . utf8_decode($assinatura_responsavel), 0, 1);
+$pdf->Cell(0, 10, utf8_decode('Assinatura do Responsável: ' . $assinatura_responsavel), 0, 1);
 
 $pdf->Output('D', 'termo_autorizacao.pdf');
 ?>
