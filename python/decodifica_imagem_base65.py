adc0997fb4953b5b97d21fe1b60fdbbebef1ef9d
Matheus <matheus_valladao@hotmail.com>
2024-08-30 07:24:18 -0300

    Marcação de Serviço Extra

--- python/decodifica_imagem_base65.py
+++ python/decodifica_imagem_base65.py
@@ -8,33 +8,26 @@ image = Image.open('captcha_image.png')
 image = image.convert('L')
 
 # Ajustar o brilho e o contraste
-#enhancer = ImageEnhance.Contrast(image)
-#image = enhancer.enhance(2.5)  # Aumentar o contraste
+enhancer = ImageEnhance.Contrast(image)
+image = enhancer.enhance(2.0)  # Ajuste do contraste
 
-#brightness = ImageEnhance.Brightness(image)
-#image = brightness.enhance(2.2)  # Aumentar o brilho
+brightness = ImageEnhance.Brightness(image)
+image = brightness.enhance(2.0)  # Ajuste do brilho
 
-# Aplicar filtro de Unsharp Mask para realçar bordas
-image = image.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=3))
+# Aplicar filtro para redução de ruído
+image = image.filter(ImageFilter.MedianFilter(size=3))
+
+# Redimensionar a imagem antes de aplicar os filtros
+image = image.resize((int(image.width * 1.5), int(image.height * 1.5)), Image.Resampling.BOX)
 
 # Binarizar a imagem (conversão para preto e branco)
-threshold = 128  # Ajuste o valor conforme necessário
+threshold = 150  # Ajuste fino do limite
 image = image.point(lambda p: p > threshold and 255)
-
-# Redimensionar a imagem
-image = image.resize((image.width * 2, image.height * 2), Image.Resampling.BOX)
-
-# Inverter as cores
-#image = ImageOps.invert(image)
-
-# Binarizar a imagem
-#image = image.point(lambda p: p > 100 and 255)
 
 # Mostrar a imagem processada
 image.show()
 
-# Usar o pytesseract para extrair o texto da imagem
-captcha_text = pytesseract.image_to_string(image, config='--psm 6 --oem 1', lang='eng')
+# Usar o pytesseract para extrair o texto da imagem com whitelist de caracteres
+captcha_text = pytesseract.image_to_string(image, config='--psm 7 --oem 1 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 
 print("Texto extraído do CAPTCHA:", captcha_text)
-
