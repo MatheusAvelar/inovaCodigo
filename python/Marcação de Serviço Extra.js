2a6d65605591d96a4c9b18fcfdf6c75110cd7c44
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-09-23 07:34:58 -0300

    Boot para marcação de horario

--- python/Marcação de Serviço Extra.js
+++ python/Marcação de Serviço Extra.js
@@ -1,8 +1,7 @@
 document.getElementById('ddlConvenios').value = '60';
 document.getElementById('ddlConvenios').dispatchEvent(new Event('change'));
-document.getElementById('ddlDataEvento').value = '78'; //Data 82 - 83 - 84 - 85 - 86
+document.getElementById('ddlDataEvento').value = '88'; //Data 82 - 83 - 84 - 85 - 86
 document.getElementById('ddlDataEvento').dispatchEvent(new Event('change'));
 document.getElementById('ddlCPAS').value = '37';
 document.getElementById('ddlCPAS').dispatchEvent(new Event('change'));
-// Execute esse código no contexto da página onde o link está presente
 document.getElementById('lnkNewCaptcha').click();
