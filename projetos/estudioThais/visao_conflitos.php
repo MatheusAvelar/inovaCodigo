55a7cbb59de56ed005411d749b5bf43d3e0275f7
Matheus <matheus_valladao@hotmail.com>
2024-09-26 13:09:12 -0300

    Avelart - Teste de Conflitos de Agendamentos

--- projetos/estudioThais/visao_conflitos.php
+++ projetos/estudioThais/visao_conflitos.php
@@ -17,6 +17,13 @@ if ($_SESSION['perfil_id'] != 2) {
     <script src="https://cdn.sheetjs.com/xlsx-latest/xlsx.full.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+
+    <!-- Popper.js e Bootstrap Bundle JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
     <header>
