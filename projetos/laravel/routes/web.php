b50c1aa63e30f5ca34e40fecfd49da6160a4a6ba
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-09-28 09:16:06 -0300

    Projeto CRM - Pagina de Login

--- projetos/laravel/routes/web.php
+++ projetos/laravel/routes/web.php
@@ -17,12 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-
 require __DIR__.'/auth.php';
