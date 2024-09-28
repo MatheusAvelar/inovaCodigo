bb70de70669d5bbbc79a6841ca92a8b57e838417
Matheus <matheus_valladao@hotmail.com>
2024-09-27 22:21:51 -0300

    Projeto CRM - Tela de Login

--- projetos/laravel/resources/views/login.blade.php
+++ projetos/laravel/resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - CRM</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!--<link rel="stylesheet" href="{{ asset('css/app.css') }}">-->
     <style>
         /* Estilos básicos para a página de login */
         body {
