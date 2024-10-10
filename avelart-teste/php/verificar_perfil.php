2198132113dd306b27fcb2820a91e628ec64b18f
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2024-10-10 09:59:15 -0300

    Avelart - Relatórios

--- avelart-teste/php/verificar_perfil.php
+++ avelart-teste/php/verificar_perfil.php
@@ -1,30 +1,28 @@
 <?php
-include 'utils.php';
-
-try {
-    $conn = conectaBanco();
-} catch (Exception $e) {
-    die("Erro: " . $e->getMessage());
-}
-
 // Verifica se o usuário está logado, se não, redireciona para a página de login
-/*if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header("Location: index.html");
     exit;
-}*/
+}
+
+// Conecta ao banco de dados para obter o perfil do usuário
+$conexao = mysqli_connect("127.0.0.1:3306", "u221588236_root", "Camila@307", "u221588236_controle_finan");
+
+if (!$conexao) {
+    die("Falha na conexão: " . mysqli_connect_error());
+}
 
 // Obtém o perfil do usuário
 $usuario_id = $_SESSION['id'];
 $sql = "SELECT perfil_id FROM usuarioEstudio WHERE id = '$usuario_id'";
-$resultado = mysqli_query($conn, $sql);
+$resultado = mysqli_query($conexao, $sql);
 
 if ($resultado) {
     $usuario = mysqli_fetch_assoc($resultado);
     $perfil_id = $usuario['perfil_id'];
-    echo "Perfil: ".$perfil_id;
 } else {
-    die("Erro ao obter perfil do usuário: " . mysqli_error($conn));
+    die("Erro ao obter perfil do usuário: " . mysqli_error($conexao));
 }
 
-mysqli_close($conn);
+mysqli_close($conexao);
 ?>
