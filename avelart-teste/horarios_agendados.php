462db9c28f1b104a7480ae4faaed2634d054996e
Matheus Avelar <matheus_valladao@hotmail.com>
2024-11-19 13:22:35 -0300

    Ajuste para relatórios

--- avelart-teste/horarios_agendados.php
+++ avelart-teste/horarios_agendados.php
@@ -137,7 +137,7 @@ unset($_SESSION['status'], $_SESSION['message']);
                 <option value="">Todos os Tatuadores</option>
                 <?php
                 // Carregar a lista de tatuadores
-                include 'php/get_tatuadores.php';
+                //include 'php/get_tatuadores.php';
                 ?>
             </select>
 
