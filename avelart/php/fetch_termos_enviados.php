936f94174ab98353e9e990ccbcafb3cb0c5cb4da
Matheus <matheus_valladao@hotmail.com>
2024-11-25 17:26:27 -0300

    Contagem de Registros - Avelart

--- avelart/php/fetch_termos_enviados.php
+++ avelart/php/fetch_termos_enviados.php
@@ -19,14 +19,6 @@ if ($conn->connect_error) {
 $cliente_nome = isset($_GET['cliente_nome']) ? trim($_GET['cliente_nome']) : '';
 
 if($perfilUsuario == 2){
-    // Consulta para contar o número total de registros ativos
-    /*$count_query = "SELECT COUNT(*) AS total FROM termos_enviados WHERE status = 'ativo'";
-    $count_result = $mysqli->query($count_query);
-    $total_records = 0;
-    if ($count_result) {
-        $total_records = $count_result;
-    }*/
-
     $sql = "SELECT 
                 id, 
                 CONCAT(UPPER(SUBSTRING(nome_cliente, 1, 1)), LOWER(SUBSTRING(nome_cliente, 2))) AS nome_cliente, 
@@ -35,14 +27,6 @@ if($perfilUsuario == 2){
             FROM termos_enviados 
             WHERE status = 'ativo'";
 } else {
-    // Consulta para contar o número total de registros ativos
-    /*$count_query = "SELECT COUNT(*) AS total FROM termos_enviados WHERE status = 'ativo' AND usuario_id = $usuarioLogado";
-    $count_result = $mysqli->query($count_query);
-    $total_records = 0;
-    if ($count_result) {
-        $total_records = $count_result;
-    }*/
-
     $sql = "SELECT 
                 id,
                 CONCAT(UPPER(SUBSTRING(nome_cliente, 1, 1)), LOWER(SUBSTRING(nome_cliente, 2))) AS nome_cliente,email_cliente,
