e4d6e3ab9a169509e9206dd26f25b139b9109a22
Matheus <matheus_valladao@hotmail.com>
2024-11-29 12:55:15 -0300

    Alteração de Filtro - Avelart

--- avelart-teste/horarios_agendados.php
+++ avelart-teste/horarios_agendados.php
@@ -8,6 +8,21 @@ $message = isset($_SESSION['message']) ? $_SESSION['message'] : null;
 
 // Limpa as mensagens de status da sessão após exibir
 unset($_SESSION['status'], $_SESSION['message']);
+
+$months = [
+    "01" => "Janeiro",
+    "02" => "Fevereiro",
+    "03" => "Março",
+    "04" => "Abril",
+    "05" => "Maio",
+    "06" => "Junho",
+    "07" => "Julho",
+    "08" => "Agosto",
+    "09" => "Setembro",
+    "10" => "Outubro",
+    "11" => "Novembro",
+    "12" => "Dezembro",
+];
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
