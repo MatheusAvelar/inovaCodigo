f7367bf0c88c89091f8515e1a4708b6bada5c195
Matheus <matheus_valladao@hotmail.com>
2024-12-06 15:17:57 -0300

    Avelart - Relatório

--- avelart-teste/php/get_dados_relatorio.php
+++ avelart-teste/php/get_dados_relatorio.php
@@ -54,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $params[] = $tatuador;
     }
     if (!empty($inicio) && !empty($fim)) {
-        $param_types .= 'ii';
+        $param_types .= 'ss';
         $params[] = $inicio;
         $params[] = $fim;
     }
