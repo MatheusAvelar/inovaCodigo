d03ddc2ac29d09cf5e798f6a1bc5f86611dc30bd
Matheus <matheus_valladao@hotmail.com>
2024-12-06 15:04:48 -0300

    Inova Codigo - Adsense

--- avelart-teste/php/get_dados_relatorio.php
+++ avelart-teste/php/get_dados_relatorio.php
@@ -18,22 +18,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fim = $_POST['fim'];
     $tatuador = $_POST['filter_tatuador'];
     $opcao_total = $_POST['opcao_total'];
-
-    // Construir a consulta SQL
-    /*$query = "SELECT usuario_id, SUM(valor) AS total_faturado, SUM(valor) AS total_estudio
-              FROM agendamentos
-              WHERE data BETWEEN '$inicio' AND '$fim'";*/
-    $query = "SELECT usuario_id, SUM(valor) AS total_faturado, SUM(valor) AS total_estudio
-            FROM agendamentos
-            GROUP BY usuario_id";
+    
+    $query = "SELECT 
+                    CONCAT(UCASE(LEFT(ue.nome, 1)), LCASE(SUBSTRING(ue.nome, 2)), ' ', 
+                        UCASE(LEFT(ue.sobrenome, 1)), LCASE(SUBSTRING(ue.sobrenome, 2))) AS nome_completo,
+                    SUM(a.valor) AS total_faturado,
+                    (SELECT SUM(valor) FROM agendamentos) AS total_estudio
+                FROM 
+                    agendamentos a
+                JOIN 
+                    usuarioEstudio ue
+                ON 
+                    a.usuario_id = ue.id";
 
     // Filtrar por tatuador se não for 'Todos'
-    /*if ($tatuador != '') {
+    if ($tatuador != '') {
         $query .= " AND usuario_id = $tatuador";
     }
 
-    $query .= " GROUP BY usuario_id";*/
-
+    $query .= " GROUP BY ue.id, ue.nome, ue.sobrenome";
+    
     // Executar a consulta
     $result = $conn->query($query);
 
@@ -43,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <tr>
                     <th>Tatuador</th>";
         if ($opcao_total == 'faturado') {
-            echo "<th>Total Faturado</th>";
+            echo "<th>Total Faturado por " . $row['nome_completo'] . "</th>";
         } else {
             echo "<th>Total Recebido pelo Estúdio</th>";
         }
@@ -52,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Exibir os dados
         while ($row = $result->fetch_assoc()) {
             echo "<tr>
-                    <td>" . $row['usuario_id'] . "</td>";
+                    <td>" . $row['nome_completo'] . "</td>";
             if ($opcao_total == 'faturado') {
                 echo "<td>R$ " . number_format($row['total_faturado'], 2, ',', '.') . "</td>";
             } else {
