47e18ac38d75a0fcf639f17bf0408809e2b696cb
Matheus <matheus_valladao@hotmail.com>
2024-12-06 16:04:21 -0300

    Avelart - Relatório

--- avelart-teste/relatorios.php
+++ avelart-teste/relatorios.php
@@ -14,6 +14,21 @@ $message = isset($_SESSION['message']) ? $_SESSION['message'] : null;
 
 // Limpa as mensagens de status da sessão após exibir
 unset($_SESSION['status'], $_SESSION['message']);
+
+$months = [
+    "01" => "Janeiro",
+    "02" => "Fevereiro",
+    "03" => "Março",
+    "04" => "Abril",
+    "05" => "Maio",
+    "06" => "Junho",
+    "07" => "Julho",
+    "08" => "Agosto",
+    "09" => "Setembro",
+    "10" => "Outubro",
+    "11" => "Novembro",
+    "12" => "Dezembro",
+];
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -113,17 +128,35 @@ unset($_SESSION['status'], $_SESSION['message']);
         </div>
 
         <h2>Relatório</h2>
+        <form id="filter-actions-form" method="GET" action="horarios_agendados.php">
+            <div id="filters-container">
+                <div>
+                    <label for="filter-month">Mês:</label>
+                    <select id="filter-month" name="filter_month">
+                        <option value="">Todos os Meses</option>
+                        <?php
+                        foreach ($months as $value => $name) {
+                            $selected = (isset($_GET['filter_month']) && $_GET['filter_month'] == $value) ? 'selected' : '';
+                            echo "<option value=\"$value\" $selected>$name</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
 
-        <div class="grid">
-            <div class="maca">
-                <form method="POST" action="php/get_dados_relatorio.php">
-                    <label for="inicio">Data de Início:</label>
-                    <input type="date" id="inicio" name="inicio">
+                <div>
+                    <label for="filter-maca">Maca:</label>
+                    <select id="filter-maca" name="filter_maca">
+                        <option value="">Todas as Macas</option>
+                        <option value="1" <?= isset($_GET['filter_maca']) && $_GET['filter_maca'] == '1' ? 'selected' : '' ?>>Maca 1</option>
+                        <option value="2" <?= isset($_GET['filter_maca']) && $_GET['filter_maca'] == '2' ? 'selected' : '' ?>>Maca 2</option>
+                        <option value="3" <?= isset($_GET['filter_maca']) && $_GET['filter_maca'] == '3' ? 'selected' : '' ?>>Maca 3</option>
+                        <option value="4" <?= isset($_GET['filter_maca']) && $_GET['filter_maca'] == '4' ? 'selected' : '' ?>>Maca 4</option>
+                        <option value="5" <?= isset($_GET['filter_maca']) && $_GET['filter_maca'] == '5' ? 'selected' : '' ?>>Sala de Atendimento Íntimo</option>
+                    </select>
+                </div>
 
-                    <label for="fim">Data de Fim:</label>
-                    <input type="date" id="fim" name="fim">
-
-                    <label for="filter_tatuador">Tatuador:</label>
+                <div>
+                    <label for="filter-tatuador">Tatuador:</label>
                     <select id="filter_tatuador" name="filter_tatuador">
                         <option value="">Todos os Tatuadores</option>
                         <?php
@@ -148,53 +181,73 @@ unset($_SESSION['status'], $_SESSION['message']);
                         }
                         ?>
                     </select>
+                </div>
 
-                    <label for="tipo_relatorio">Tipo de Relatório:</label>
-                    <select id="tipo_relatorio" name="tipo_relatorio">
-                        <option value="faturado">Total Faturado</option>
-                        <option value="recebido_estudio">Total Recebido pelo Estúdio</option>
-                    </select>
+                <?php if ($perfil_id == 2) : ?>
+                    <div>
+                        <label for="filter-status">Status:</label>
+                        <select id="filter-status" name="filter_status">
+                            <option value="1">Ativo</option>
+                            <option value="0">Inativo</option>
+                        </select>
+                    </div>
+                <?php endif; ?>
 
-                    <button type="submit">Gerar Relatório</button>
-                </form>
-                <!-- Div para exibir os resultados -->
-                <div id="resultado" style="margin-top: 20px; border: 1px solid #ddd; padding: 10px; display: none;">
-                    <h2>Resultado do Relatório</h2>
-                    <div id="dados-relatorio"></div>
+                <label for="inicio">Data de Início:</label>
+                <input type="date" id="inicio" name="inicio">
+
+                <label for="fim">Data de Fim:</label>
+                <input type="date" id="fim" name="fim">
+
+                <label for="tipo_relatorio">Tipo de Relatório:</label>
+                <select id="tipo_relatorio" name="tipo_relatorio">
+                    <option value="faturado">Total Faturado</option>
+                    <option value="recebido_estudio">Total Recebido pelo Estúdio</option>
+                </select>
+            </div>
+
+            <div id="actions-container">
+                <button type="submit" class="button">
+                    <i class="fas fa-search"></i> Filtrar
+                </button>
+
+                <?php if ($perfil_id == 2) : ?>
+                    <button type="button" class="button" id="export-button">
+                        <i class="fa-solid fa-file-csv"></i> Exportar
+                    </button>
+                <?php endif; ?>
+            </div>
+        </form>
+
+        <div class="grid">
+            <div class="maca">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tatuador</th>
+                            <th>Valor</th>
+                            <th>Tipo Relatório</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $filter_date = $_GET['filter_date'] ?? '';
+                        $filter_maca = $_GET['filter_maca'] ?? '';
+
+                        // Inclua o arquivo com a lógica para buscar agendamentos com filtros
+                        include 'php/get_dados_relatorio_tatuador.php';
+                        ?>
+                    </tbody>
+                </table><br>
+                <!-- Exibe a contagem de registros -->
+                <div class="record-count">
+                    <?php echo "Total de Registros: " . $total_records; ?>
                 </div>
             </div>
         </div>
+
+        
     </div>
-    <script>
-        // Evento de envio do formulário
-        $('#form-relatorio').on('submit', function(e) {
-            e.preventDefault();
-
-            // Obter o tipo de relatório selecionado
-            const tipoRelatorio = $('#tipo_relatorio').val();
-
-            if (!tipoRelatorio) {
-                alert("Por favor, selecione um tipo de relatório.");
-                return;
-            }
-
-            // Enviar via AJAX para o servidor
-            $.ajax({
-                url: 'get_dados_relatorio.php', // Arquivo PHP para processar
-                type: 'POST',
-                data: {
-                    tipo_relatorio: tipoRelatorio
-                },
-                success: function(response) {
-                    $('#dados-relatorio').html(response); // Atualizar a div com os dados
-                    $('#resultado').show(); // Exibir a div de resultados
-                },
-                error: function() {
-                    alert("Erro ao gerar relatório. Tente novamente.");
-                }
-            });
-        });
-    </script>
 </body>
 
 </html>
