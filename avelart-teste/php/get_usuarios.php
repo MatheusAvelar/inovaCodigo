b6bec0e569e3ed869ff6e9f1474e6dc8d25141b4
Matheus Avelar <matheus_valladao@hotmail.com>
2024-12-10 10:39:46 -0300

    Paginação - Avelart

--- avelart-teste/php/get_usuarios.php
+++ avelart-teste/php/get_usuarios.php
@@ -38,10 +38,11 @@ $totalRecordsCurrentPage = $result->num_rows;
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
-        //echo "<td>" . htmlspecialchars($row['id']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['id']) . "</td>";
         $ativoStatus = $row['ativo'] == 1 ? 'Ativo' : 'Inativo';
         echo "<td>" . htmlspecialchars($ativoStatus) . "</td>";
-        echo "<td>" . htmlspecialchars($row['nome']) . " " .htmlspecialchars($row['sobrenome']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['nome']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['sobrenome']) . "</td>";
         echo "<td>" . htmlspecialchars($row['email']) . "</td>";
         echo "<td>" . htmlspecialchars($row['perfil_nome']) . "</td>";
         echo "<td>
