b462c174c5e4a32b6de2711d14bf464c65ef16ba
Matheus Avelar <matheus_valladao@hotmail.com>
2024-12-10 14:13:41 -0300

    Paginação/Filtros - Avelart

--- avelart-teste/termos_enviados.php
+++ avelart-teste/termos_enviados.php
@@ -294,19 +294,19 @@ $months = [
                 <!-- Exibe a lista de páginas -->
                 <div class="pagination">
                     <?php if ($currentPage > 1): ?>
-                        <a href="?page=1&cliente_nome=<?php echo $cliente_nome; ?>" class="page-link"><i class="fas fa-angles-left"></i></a>
-                        <a href="?page=<?php echo $currentPage - 1; ?>&cliente_nome=<?php echo $cliente_nome; ?>" class="page-link"><i class="fas fa-angle-left"></i></a>
+                        <a href="?page=1&cliente_nome=<?php echo $cliente_nome; ?>&filter_month=<?php echo $filter_month; ?>" class="page-link"><i class="fas fa-angles-left"></i></a>
+                        <a href="?page=<?php echo $currentPage - 1; ?>&cliente_nome=<?php echo $cliente_nome; ?>&filter_month=<?php echo $filter_month; ?>" class="page-link"><i class="fas fa-angle-left"></i></a>
                     <?php endif; ?>
 
                     <?php for ($i = 1; $i <= $totalPages; $i++): ?>
-                        <a href="?page=<?php echo $i; ?>&cliente_nome=<?php echo $cliente_nome; ?>" class="page-link <?php echo $i == $currentPage ? 'active' : ''; ?>">
+                        <a href="?page=<?php echo $i; ?>&cliente_nome=<?php echo $cliente_nome; ?>&filter_month=<?php echo $filter_month; ?>" class="page-link <?php echo $i == $currentPage ? 'active' : ''; ?>">
                             <?php echo $i; ?>
                         </a>
                     <?php endfor; ?>
 
                     <?php if ($currentPage < $totalPages): ?>
-                        <a href="?page=<?php echo $currentPage + 1; ?>&cliente_nome=<?php echo $cliente_nome; ?>" class="page-link"><i class="fas fa-angle-right"></i></a>
-                        <a href="?page=<?php echo $totalPages; ?>&cliente_nome=<?php echo $cliente_nome; ?>" class="page-link"><i class="fas fa-angles-right"></i></a>
+                        <a href="?page=<?php echo $currentPage + 1; ?>&cliente_nome=<?php echo $cliente_nome; ?>&filter_month=<?php echo $filter_month; ?>" class="page-link"><i class="fas fa-angle-right"></i></a>
+                        <a href="?page=<?php echo $totalPages; ?>&cliente_nome=<?php echo $cliente_nome; ?>&filter_month=<?php echo $filter_month; ?>" class="page-link"><i class="fas fa-angles-right"></i></a>
                     <?php endif; ?>
                 </div>
                 <br>
