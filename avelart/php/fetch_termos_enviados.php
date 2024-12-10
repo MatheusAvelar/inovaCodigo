3c8011537eea58279421db9d8f0d08374c251704
Matheus Avelar <matheus_valladao@hotmail.com>
2024-12-10 10:26:38 -0300

    Paginação - Avelart

--- avelart/php/fetch_termos_enviados.php
+++ avelart/php/fetch_termos_enviados.php
@@ -90,7 +90,7 @@ $totalRecords = $total_result->fetch_row()[0];
 
 // Calcular o número total de páginas
 $totalPages = ceil($totalRecords / $perPage);
-echo $sql;
+
 // Consulta principal para buscar os registros com paginação
 $result = $conn->query($sql);
 
