ad96266584a3ab5901423158fa1d0035a1304de6
Matheus Avelar <matheus_valladao@hotmail.com>
2024-12-10 10:28:43 -0300

    Paginação - Avelart

--- avelart/horarios_agendados.php
+++ avelart/horarios_agendados.php
@@ -308,8 +308,8 @@ $months = [
                 <!-- Exibe a lista de páginas -->
                 <div class="pagination">
                     <?php if ($currentPage > 1): ?>
-                        <a href="?page=1" class="page-link"><i class="fas fa-angle-double-left"></i></a>
-                        <a href="?page=<?php echo $currentPage - 1; ?>" class="page-link"><i class="fas fa-arrow-left"></i></a>
+                        <a href="?page=1" class="page-link"><i class="fas fa-angles-left"></i></a>
+                        <a href="?page=<?php echo $currentPage - 1; ?>" class="page-link"><i class="fas fa-angle-left"></i></a>
                     <?php endif; ?>
 
                     <?php for ($i = 1; $i <= $totalPages; $i++): ?>
@@ -319,8 +319,8 @@ $months = [
                     <?php endfor; ?>
 
                     <?php if ($currentPage < $totalPages): ?>
-                        <a href="?page=<?php echo $currentPage + 1; ?>" class="page-link"><i class="fas fa-arrow-right"></i></a>
-                        <a href="?page=<?php echo $totalPages; ?>" class="page-link"><i class="fas fa-angle-double-right"></i></a>
+                        <a href="?page=<?php echo $currentPage + 1; ?>" class="page-link"><i class="fas fa-angle-right"></i></a>
+                        <a href="?page=<?php echo $totalPages; ?>" class="page-link"><i class="fas fa-angles-right"></i></a>
                     <?php endif; ?>
                 </div>
                 <br>
