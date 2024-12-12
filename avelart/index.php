ffe2edcdecb21301f518d9a00b9425a84fde2d6e
Matheus Avelar <matheus_valladao@hotmail.com>
2024-12-12 20:20:15 -0300

    Pagina Home

--- avelart/index.php
+++ avelart/index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Agendamento</title>
+    <link rel="icon" href="img/ico.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
         #menu ul li { 
             display: inline-block; 
