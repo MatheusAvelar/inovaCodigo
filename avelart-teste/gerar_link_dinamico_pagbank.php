f2ac0a6980e932fd9616ca4925c6d74ca6844b7e
Matheus <matheus_valladao@hotmail.com>
2024-12-17 16:11:48 -0300

    Pagamento

--- avelart-teste/gerar_link_dinamico_pagbank.php
+++ avelart-teste/gerar_link_dinamico_pagbank.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Credenciais da API PagSeguro
     $pagbank_email = "matheus_valladao@hotmail.com";
     $pagbank_token = "75acd1e9-fb07-4c42-96ff-3ec516f8fe4c894e4de44aceb872f623a723dc9142b455e8-f737-427d-8970-945a76961132";
-    $api_url = "https://sandbox.api.pagseguro.com";
+    $api_url = "https://sandbox.api.pagseguro.com/v2/checkout";
 
     // Valida o valor recebido
     $valor = floatval($_POST['valor'] ?? 0);
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Processa a resposta da API
             $xml = simplexml_load_string($result);
             if ($xml && isset($xml->code)) {
-                $paymentLink = "https://sandbox.api.pagseguro.com/payment.html?code={$xml->code}";
+                $paymentLink = "https://pagseguro.uol.com.br/v2/checkout/payment.html?code={$xml->code}";
                 $response = ['success' => true, 'payment_url' => $paymentLink];
             } else {
                 $response = ['success' => false, 'message' => 'Resposta inválida da API.', 'debug' => $result];
