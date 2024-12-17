10dea9c02b44e3be1f26f6ed4ba68dcccd441ec4
Matheus <matheus_valladao@hotmail.com>
2024-12-17 15:15:06 -0300

    phpinfo()

--- avelart-teste/php/processa_pagamento_pagbank.php
+++ avelart-teste/php/processa_pagamento_pagbank.php
@@ -1,6 +1,18 @@
 <?php
 header('Content-Type: application/json');
 
+$url = "https://ws.pagseguro.uol.com.br/v2/checkout";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo "Erro cURL: " . curl_error($ch);
+} else {
+    echo "Conexão com PagBank estabelecida com sucesso!";
+}
+curl_close($ch);
+
 // Credenciais da API PagSeguro
 $pagbank_email = "matheus_valladao@hotmail.com";
 $pagbank_token = "75acd1e9-fb07-4c42-96ff-3ec516f8fe4c894e4de44aceb872f623a723dc9142b455e8-f737-427d-8970-945a76961132";
