b16eb83fc8cc7aeda33841607f4015d3bf7736e8
Matheus Avelar <matheus_valladao@hotmail.com>
2024-12-18 12:59:27 -0300

    Pagamento

--- avelart-teste/gerar_pagamento.php
+++ avelart-teste/gerar_pagamento.php
@@ -141,7 +141,7 @@ unset($_SESSION['status'], $_SESSION['message']);
                 const res = await fetch('php/processa_pagamento_pagbank.php', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                    body: `valor=${encodeURIComponent(valor)}`
+                    body: `valor=${encodeURIComponent(valor)}&nome=${encodeURIComponent(document.getElementById('nome').value)}&email=${encodeURIComponent(document.getElementById('email').value)}&telefone=${encodeURIComponent(document.getElementById('telefone').value)}&cpf=${encodeURIComponent(document.getElementById('cpf').value)}`
                 });
 
                 const data = await res.json();
