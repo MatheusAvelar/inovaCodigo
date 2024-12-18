2adfb55784bbc75b11aa0c8d1a4fab6ab54f74f5
Matheus Avelar <matheus_valladao@hotmail.com>
2024-12-18 09:06:03 -0300

    Pagamento

--- avelart-teste/gerar_link_dinamico_pagbank.php
+++ avelart-teste/gerar_link_dinamico_pagbank.php
@@ -6,20 +6,20 @@ $data = [
     'reference_id' => 'REFERENCIA123',
     'expiration_date' => '2024-12-31T19:09:10-03:00',
     'customer' => [
-        'name' => 'João Teste',
-        'email' => 'joao@teste.com',
-        'tax_id' => '12345678909',
+        'name' => '',
+        'email' => '',
+        'tax_id' => '',
         'phone' => [
             'country' => '+55',
-            'area' => '27',
-            'number' => '999999999'
+            'area' => '',
+            'number' => ''
         ]
     ],
     'customer_modifiable' => true,
     'items' => [
         [
             'reference_id' => 'ITEM01',
-            'name' => 'Nome do Produto',
+            'name' => 'Tatuagem',
             'quantity' => 1,
             'unit_amount' => 500,
             'image_url' => 'https://www.petz.com.br/blog//wp-content/upload/2018/09/tamanho-de-cachorro-pet-1.jpg'
