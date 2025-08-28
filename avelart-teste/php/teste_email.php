ac55f1e9066e068014677b555cabcbc8ab475000
Matheus Avelar <matheus_valladao@hotmail.com>
2025-08-28 13:10:54 -0300

    Teste Email

--- avelart-teste/php/teste_email.php
+++ avelart-teste/php/teste_email.php
@@ -1,4 +1,18 @@
 <?php
+include 'envia_email.php';
+
+// Configuração da conexão com o banco de dados
+$servername = "127.0.0.1:3306";
+$username = "u221588236_root";
+$password = "Camila@307";
+$dbname = "u221588236_controle_finan";
+
+// Criando a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
 // Destinatário de teste
 $to = "matheus_valladao@hotmail.com";
 $subject = "Teste de envio de e-mail";
@@ -7,7 +21,7 @@ $headers = "From: no-reply@seudominio.com\r\n" .
            "Reply-To: no-reply@seudominio.com\r\n" .
            "X-Mailer: PHP/" . phpversion();
 
-$result = mail($to, $subject, $message, $headers);
+$result = sendEmail($to, $subject, $message, $headers, $conn);
 
 // Verifica o resultado
 if ($result) {
